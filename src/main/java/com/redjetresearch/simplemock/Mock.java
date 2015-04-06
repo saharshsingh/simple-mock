b52e64597e7c990d912e828afb846546d793ff82
Saharsh Singh <saharsh.singh@gmail.com>
2015-04-06 06:49:02 +0100

    Cleaned up comments

--- src/main/java/com/redjetresearch/simplemock/Mock.java
+++ src/main/java/com/redjetresearch/simplemock/Mock.java
@@ -1,14 +1,3 @@
-/*
- * COPYRIGHT (2015) RAYTHEON COMPANY
- * ALL RIGHTS RESERVED, An Unpublished Work
- *
- * This software was developed pursuant to Contract Number (specify contract number)
- * with the US Government.
- *
- * The US Government's rights in and to this copyrighted software
- * are as specified in DFAR 252.227-7014 which is made part
- * of the above contract.
- */
 package com.redjetresearch.simplemock;
 
 import java.lang.reflect.Method;
